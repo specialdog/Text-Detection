3ee14f27214d8e1a776852a89213eca11f7cc772
specialdog <46859082+specialdog@users.noreply.github.com>
2019-08-22 15:42:08 +0800

    Add files via upload

---
+++ HOG.m
@@ -0,0 +1,23 @@
+imdsTrain = imageDatastore('E:\百度云\图像处理\text-detection\HOG_SVM',...  
+    'IncludeSubfolders',true,...  
+    'LabelSource','foldernames'); 
+Train_disp = countEachLabel(imdsTrain);
+disp(Train_disp);
+imageSize = [256,256];% 对所有图像进行此尺寸的缩放 ?
+image1 = readimage(imdsTrain,1); 
+scaleImage = imresize(image1,imageSize); 
+[features, visualization] = extractHOGFeatures(scaleImage); 
+imshow(scaleImage);hold on; plot(visualization) 
+numImages = length(imdsTrain.Files); 
+featuresTrain = zeros(numImages,size(features,2),'single'); % featuresTrain为单精度 ?
+for i = 1:numImages 
+ imageTrain = readimage(imdsTrain,i); 
+ imageTrain = imresize(imageTrain,imageSize); 
+ featuresTrain(i,:) = extractHOGFeatures(imageTrain); 
+end 
+trainLabels = imdsTrain.Labels;
+classifer = fitcsvm(featuresTrain,trainLabels,'KernelFunction','linear'); 
+save('./svm_model/model','classifer');
+
+
+
